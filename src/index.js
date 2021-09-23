d139376154a834c01724d303e684958584ff8e13
Sandra Sügav <MM-20-337@tptlive.ee>
2021-09-23 09:53:14 +0300

    java

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
-require('./styles.scss');
-const logger = require('./log.js');
-logger.log("hello");
-require('./index.ts');
-
-class hello {
-    hello(){
-        console.log("hello")
-    }
+for(let i = 0 ; i<10 ; i++){
+    console.log(i)
 }
 
-let helloObj = new hello();
-helloObj.hello();
+let tups = ['lime','cold','ice']
+for(let j = 0; j<tups.length; j++) {
+    if (tups[j] === 'lime') {
+        console.log('Mulle meeldib' + ' ' + tups[j])
+    } else {
+        console.log('Mulle ei meeldi' + ' ' + tups[j])
+    }
+}
+let num = 2;
+while(num<1000000) {
+    num*=2;
+    console.log(num)
+}
+num = 1;
+while(num>1) {
+    console.log('while:' + num);
+}
+do {
+    console.log('do:' + num);
+}while(num>1);
+
+let fruits = ['apple','cherry','pear'];
+fruits.forEach((fruit, key)=> {
+    fruits='baah';
+    console.log('key:' + key + 'value:' + fruit)
+});
+
+let newFruits = [fruits].map((fruit, key)=> {
+    return fruit+key;
+});
+
+console.log(newFruits);
+
+let nums = [1,2,3,4,5,6,7,8,9];
+let sum = nums.reduce((sum, value)=>{
+    return sum + value;
+});
+
+console.log(sum);
+nums = [5,2,3,7,4,1,9,6,8];
+let sortedNums = nums.sort((a,b) => {
+    if(a>b){
+        return 1;
+    } else if(a<b) {
+        return -1;
+    }
+    return 0;
+});
+console.log(sortedNums);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -18,41 +17,47 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.css$/, use: ['style-loader','css-loader'] },
-      { test: /\.s[ca]ss$/, use: ['style-loader','css-loader','sass-loader'] },
-      { test: /\.twig$/, use: ['twig-loader']},
-      { test: /\.ts$/, use: ['ts-loader']}
-      // {
-      //   module: {
-      //     rules: [
-      //       {
-      //         test: /\.m?js$/,
-      //         exclude: /node_modules/,
-      //         use: {
-      //           loader: "babel-loader",
-      //           options: {
-      //             presets: ['@babel/preset-env']
-      //           }
-      //         }
-      //       }
-      //     ]
-      //   }
-      // }
+        { 
+          test: /\.css$/,
+          use: ['style-loader','css-loader']
+        },
+        { 
+          test: /\.s[ca]ss$/,
+          use: ['style-loader','css-loader', 'sass-loader']
+        },
+        { 
+          test: /\.ts$/,
+          use: ['ts-loader']
+        },
+        { 
+          test: /\.twig$/,
+          use: ['twig-loader']
+        },
+        // {
+        //   test: /\.m?js$/,
+        //   exclude: /node_modules/,
+        //   use: {
+        //     loader: "babel-loader",
+        //     options: {
+        //       presets: ['@babel/preset-env']
+        //     }
+        //   }
+        // }
     ],
   },
   plugins: [
       new HtmlWebpackPlugin({
-         template: './src/views/pages/index.twig', 
-         filename:"index.html" ,
-         posts: [
-           {title: "Some title", content: "some cool content over here!"},
-           {title: "Some title", content: "some cool content over here!"},
-           {title: "Some title", content: "some cool content over here!"},
-           {title: "Some title", content: "some cool content over here!"},
-           {title: "Some title", content: "some cool content over here!"}
-         ]
-        }),
-      new HtmlWebpackPlugin({ template: './src/views/pages/about.twig', filename:"about.html" }),
-      new HtmlWebpackPlugin({ template: './src/views/pages/contact.twig', filename:"contact.html" })
+        template: './src/views/pages/index.twig',
+        filename: 'index.html',
+        posts: [
+          {title:'Lorem ipsum dolor sit amet.', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eveniet, officia aut impedit minus hic quae asperiores a dolores sunt nulla, sapiente provident blanditiis explicabo! Eaque rem ducimus harum commodi!'},
+          {title:'Eaque harum perferendis cum quis!', content: 'Voluptate consequatur doloribus aspernatur distinctio facere, architecto dignissimos amet iste assumenda vitae facilis quidem. Assumenda atque, officia, repudiandae odit, cupiditate numquam dignissimos culpa voluptatibus expedita libero sequi laudantium necessitatibus soluta?'},
+          {title:'Delectus obcaecati aspernatur magni ullam!', content: 'Qui esse, minima voluptas beatae exercitationem reprehenderit aliquam sed incidunt eius amet laborum accusantium perferendis ipsa nulla porro aperiam assumenda placeat, ab nihil saepe corrupti. Quae officiis necessitatibus laboriosam non.'},
+          {title:'Ullam quod officia vel rem!', content: 'Assumenda magnam delectus dicta nisi voluptatum maxime temporibus illo vitae dolores voluptas deserunt, quidem a explicabo odit accusamus dolore nulla debitis, libero quaerat beatae optio at vero facilis? Fugiat, error.'},
+          {title:'Quasi ratione quae voluptate. Officia.', content: 'Vel eaque eius impedit fuga consequatur sequi debitis fugiat cumque at, neque ex, quibusdam voluptatibus est quasi officiis, consectetur dolor dolorum voluptas illo architecto voluptatem. Consequuntur ullam totam asperiores fuga.'},
+        ]
+      }),
+      new HtmlWebpackPlugin({ template: './src/views/pages/about.twig', filename: 'about.html' }),
+      new HtmlWebpackPlugin({ template: './src/views/pages/contact.twig', filename: 'contact.html' })
     ],
 };
